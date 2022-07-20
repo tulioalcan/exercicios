beecb6627419071d4d4fb7441e4b238374f6f330
Túlio Mendes <tulioalcan@gmail.com>
2022-07-20 18:32:29 -0300

    exercicio 1 feito

---
+++ Fundamentos/Java-Script/scriptex1.js
@@ -0,0 +1,8 @@
+let a = 2
+let b = 4
+
+console.log ("Soma: " + (a+b));
+console.log ("subtração: " + (a-b));
+console.log ("Multiplicação: " + (a*b));
+console.log ("Divisão: " + (a/b));
+console.log ("Módulo: " + (a%b));
