e3e0904afe85c46791343d74e088836a359d9548
Túlio Mendes <tulioalcan@gmail.com>
2022-07-21 09:33:28 -0300

    exercicio 3 ok

---
+++ Fundamentos/Java-Script/scriptex2.js
@@ -0,0 +1,8 @@
+const num1 = 7
+const num2 = 8
+
+if (num1 > num2){
+console.log ("'num1' é maior que 'num2'";
+} else {
+  console.log ("'num2' é maior que 'num1'");
+};
---
+++ Fundamentos/Java-Script/scriptex3.js
@@ -0,0 +1,12 @@
+
+const a = 20;
+const b = 21;
+const c = 22;
+
+if (a > b && a . c) {
+  console.log("'a' é maior que 'b' e 'c'");
+} else  if (b > a && b > c) {
+  console.log("'b' é maior que 'a' e 'c'");
+} else { 
+  console.log("'c' é maior que 'a' e 'b'");
+};
